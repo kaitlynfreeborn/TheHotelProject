7e88e46673961d3fb70b650b2c53e9050c48d19a
Leila Abu-Robb <Leila.Abu-Robb@ou.edu>
2019-04-18 00:23:12 -0500

    Login not done but in progress

---
+++ KL_Hotel/KL_Hotel/Controllers/CustomerController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KL_Hotel.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            CustomerBusinessLayer customerBusiness = new CustomerBusinessLayer();
+            List<Customer> customer = customerBusiness.Customers.ToList();
+            return View(customer);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(string firstName, string lastName, int user_ID, string password)
+        {
+            CustomerBusinessLayer customerBusiness = new CustomerBusinessLayer();
+            Customer cust = new Customer();
+            cust.FirstName = firstName;
+            cust.LastName = lastName;
+            cust.User_ID = user_ID;
+            cust.Password = password;
+
+            //call the method in the business layer
+            customerBusiness.AddCustomer(cust);
+
+            return RedirectToAction("Index");
+
+
+        }
+    }
+}
---
+++ KL_Hotel/KL_Hotel/Models/customerBusinessLayer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+
+
+namespace KL_Hotel.Models
+{
+    public class customerBusinessLayerController : Controller
+    {
+        // GET: customerBusinessLayer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+    public void AddCustomer(Customer cust)
+    {
+        String connString = ConfigurationManager.ConnectionStrings["CustomerCon"].ConnectionString;
+        using (SqlConnection sqlCon = new SqlConnection(connString))
+        {
+            SqlCommand command = new SqlCommand("spAddCustomer", sqlCon);
+            command.CommandType = CommandType.StoredProcedure;
+
+           SqlParameter paramFirstName = new SqlParameter
+            {
+                ParameterName = "@FirstName",
+                Value = cust.FirstName
+            };
+
+            command.Parameters.Add(paramFirstName);
+
+            SqlParameter paramLastName = new SqlParameter
+            {
+                ParameterName = "@LastName",
+                Value = cust.LastName
+            };
+            command.Parameters.Add(paramLastName);
+
+            SqlParameter paramUser_ID = new SqlParameter
+            {
+                ParameterName = "@User_ID",
+                Value = cust.User_ID
+            };
+            command.Parameters.Add(paramUser_ID);
+
+            SqlParameter paramPassword = new SqlParameter
+            {
+                ParameterName = "@Password",
+                Value = cust.Password
+            };
+            command.Parameters.Add(paramPassword);
+
+            //open the connection
+            sqlCon.Open();
+            //execute the procedure
+            command.ExecuteNonQuery();
+        }
+    }
+}
