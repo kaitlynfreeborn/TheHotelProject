7535da5d7a60635fb403accd969c25a1cdaae822
Kaitlyn Freeborn <kaitlynfreeborn@ou.edu>
2019-04-18 13:32:31 -0500

    hello

---
+++ KL_Hotel/KL_Hotel/Controllers/RoomsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KL_Hotel.Models;
+
+namespace KL_Hotel.Controllers
+{
+    public class RoomsController : Controller
+    {
+        // GET: Rooms
+        public ActionResult Index()
+        {
+            Rooms King = new Rooms()
+            {
+                RoomType = "King Suite",
+                RoomDescription = "One king bed, and other amenities",
+                RoomImage = @"https://hiltongardeninn3.hilton.com/resources/media/gi/OKCNOGI/en_US/img/shared/full_page_image_gallery/main/GI_king001_7_698x390_FitToBoxSmallDimension_Center.jpg"
+            };
+
+            Rooms Queen = new Rooms()
+            {
+                RoomType = "Queen Suite",
+                RoomDescription = "Two queen beds, and other amenities",
+                RoomImage = @"https://hiltongardeninn3.hilton.com/resources/media/gi/OKCNOGI/en_US/img/shared/full_page_image_gallery/main/GI_dblqueen001_5_698x390_FitToBoxSmallDimension_Center.jpg"
+            };
+
+            List<Rooms> rm = new List<Rooms>();
+            rm.Add(King);
+            rm.Add(Queen);
+
+
+            return View(rm);
+        }
+
+        public ActionResult Room()
+        {
+            Rooms n = new Rooms();
+
+            return View(n);
+        }
+
+    }
+
+}
---
+++ KL_Hotel/KL_Hotel/Models/Rooms.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KL_Hotel.Models
+{
+    public class Rooms
+    {
+        public string RoomType { get; set; }
+        public string RoomDescription { get; set; }
+        public string RoomImage { get; set; }
+    }
+}
