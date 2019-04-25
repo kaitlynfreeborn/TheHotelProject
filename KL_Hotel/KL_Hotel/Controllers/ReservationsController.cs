ab6cbd87a4b0ef1db47f4e0467a667221714b10f
Leila Abu-Robb <Leila.Abu-Robb@ou.edu>
2019-04-25 13:57:21 -0500

    commented

--- KL_Hotel/KL_Hotel/Controllers/ReservationsController.cs
+++ KL_Hotel/KL_Hotel/Controllers/ReservationsController.cs
@@ -1,138 +1,138 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data.OleDb;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using KL_Hotel.Models;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Configuration;
+//using System.Data.OleDb;
+//using System.Linq;
+//using System.Web;
+//using System.Web.Mvc;
+//using KL_Hotel.Models;
 
-namespace KL_Hotel.Models
-{
-    public class ReservationsController : Controller
-    {
-        //Get Reservation
-        public ActionResult Index()
-        {
-            CustomerBusinessLayer customerBusiness = new CustomerBusinessLayer();
-            List<Reservations> reservations = customerBusiness.Reservations.ToList();
-            return View(reservations);
-        }
+//namespace KL_Hotel.Models
+//{
+//    public class ReservationsController : Controller
+//    {
+//        //Get Reservation
+//        public ActionResult Index()
+//        {
+//            CustomerBusinessLayer customerBusiness = new CustomerBusinessLayer();
+//            List<Reservations> reservations = customerBusiness.Reservations.ToList();
+//            return View(reservations);
+//        }
 
-        [HttpGet]
-        public ActionResult BookReservation()
-        {
-            return View();
-        }
+//        [HttpGet]
+//        public ActionResult BookReservation()
+//        {
+//            return View();
+//        }
 
-        [HttpPost]
-        public ActionResult BookReservation(DateTime StartDate, DateTime EndDate, string RoomType)
-        {
+//        [HttpPost]
+//        public ActionResult BookReservation(DateTime StartDate, DateTime EndDate, string RoomType)
+//        {
 
-            Reservations res = new Reservations
-            {
-                StartDate = StartDate,
-                EndDate = EndDate,
-                RoomType = RoomType
-            };
+//            Reservations res = new Reservations
+//            {
+//                StartDate = StartDate,
+//                EndDate = EndDate,
+//                RoomType = RoomType
+//            };
 
-            CustomerBusinessLayer customerBusiness = new CustomerBusinessLayer();
+//            CustomerBusinessLayer customerBusiness = new CustomerBusinessLayer();
 
-            //call the method in the business layer
-            customerBusiness.AddReservation(res);
+//            //call the method in the business layer
+//            customerBusiness.AddReservation(res);
 
-            return RedirectToAction("Index");
-
-
-        }
+//            return RedirectToAction("Index");
 
 
-        [HttpGet]
-        public ActionResult Edit(int id)
-        {
-            CustomerBusinessLayer cbl = new CustomerBusinessLayer();
-
-            //fetches all the values into the specific object with customer (gets all info so don't have to type it all out)
-           Reservations reservation = cbl.Reservations.Single(res => res.ReservationID == id);
-
-            return View(reservation);
-        }
+//        }
 
 
-        [HttpPost]
-        public ActionResult Edit(int id, string firstName, string lastName, string userName, string password)
-        {
-            Customer cust = new Customer()
-            {
-                CustomerID = id,
-                FirstName = firstName,
-                LastName = lastName,
-                UserName = userName,
-                Password = password
-            };
+//        [HttpGet]
+//        public ActionResult Edit(int id)
+//        {
+//            CustomerBusinessLayer cbl = new CustomerBusinessLayer();
 
-            CustomerBusinessLayer cbl = new CustomerBusinessLayer();
-            cbl.EditCustomer(cust);
+//            //fetches all the values into the specific object with customer (gets all info so don't have to type it all out)
+//           Reservations reservation = cbl.Reservations.Single(res => res.ReservationID == id);
 
-            return RedirectToAction("Index");
-        }
-        [HttpGet]
-        public ActionResult Delete(int id)
-        {
-            CustomerBusinessLayer cbl = new CustomerBusinessLayer();
-
-            //fetches all the values into the specific object with customer (gets all info so don't have to type it all out)
-            Customer customer = cbl.Customers.Single(cust => cust.CustomerID == id);
-
-            return View(customer);
-        }
+//            return View(reservation);
+//        }
 
 
-        [HttpPost]
-        public ActionResult Delete(int id, string confirmdelete)
-        {
-            Customer cust = new Customer()
-            {
-                CustomerID = id,
+//        [HttpPost]
+//        public ActionResult Edit(int id, string firstName, string lastName, string userName, string password)
+//        {
+//            Customer cust = new Customer()
+//            {
+//                CustomerID = id,
+//                FirstName = firstName,
+//                LastName = lastName,
+//                UserName = userName,
+//                Password = password
+//            };
 
-            };
+//            CustomerBusinessLayer cbl = new CustomerBusinessLayer();
+//            cbl.EditCustomer(cust);
 
-            CustomerBusinessLayer cbl = new CustomerBusinessLayer();
-            cbl.DeleteCustomer(cust);
+//            return RedirectToAction("Index");
+//        }
+//        [HttpGet]
+//        public ActionResult Delete(int id)
+//        {
+//            CustomerBusinessLayer cbl = new CustomerBusinessLayer();
 
-            return RedirectToAction("Index");
-        }
-        [HttpGet]
-        public ActionResult LogIn()
-        {
-            return View();
-        }
+//            //fetches all the values into the specific object with customer (gets all info so don't have to type it all out)
+//            Customer customer = cbl.Customers.Single(cust => cust.CustomerID == id);
 
-        [HttpPost]
-        public ActionResult Login(string UserName, string Password)
-        {
-
-            string connStr = ConfigurationManager.ConnectionStrings["AddCustInfo"].ConnectionString;
-            OleDbConnection oleDbConnection = new OleDbConnection(connStr);
-            oleDbConnection.Open();
-
-            OleDbCommand com = new OleDbCommand("SELECT * FROM Login WHERE [User_ID] ='" + UserName
-                + "' AND [Password]='" + Password + "'", oleDbConnection);
-
-            OleDbDataReader reader = com.ExecuteReader();
-            if (reader.HasRows)
-            {
-                Response.Write("Welcome user");
-
-            }
-            else
-            {
-                Response.Write("Invalid username/password");
-
-            }
-            return RedirectToAction("Index");
+//            return View(customer);
+//        }
 
 
-        }
-    }
-}
+//        [HttpPost]
+//        public ActionResult Delete(int id, string confirmdelete)
+//        {
+//            Customer cust = new Customer()
+//            {
+//                CustomerID = id,
+
+//            };
+
+//            CustomerBusinessLayer cbl = new CustomerBusinessLayer();
+//            cbl.DeleteCustomer(cust);
+
+//            return RedirectToAction("Index");
+//        }
+//        [HttpGet]
+//        public ActionResult LogIn()
+//        {
+//            return View();
+//        }
+
+//        [HttpPost]
+//        public ActionResult Login(string UserName, string Password)
+//        {
+
+//            string connStr = ConfigurationManager.ConnectionStrings["AddCustInfo"].ConnectionString;
+//            OleDbConnection oleDbConnection = new OleDbConnection(connStr);
+//            oleDbConnection.Open();
+
+//            OleDbCommand com = new OleDbCommand("SELECT * FROM Login WHERE [User_ID] ='" + UserName
+//                + "' AND [Password]='" + Password + "'", oleDbConnection);
+
+//            OleDbDataReader reader = com.ExecuteReader();
+//            if (reader.HasRows)
+//            {
+//                Response.Write("Welcome user");
+
+//            }
+//            else
+//            {
+//                Response.Write("Invalid username/password");
+
+//            }
+//            return RedirectToAction("Index");
+
+
+//        }
+//    }
+//}
