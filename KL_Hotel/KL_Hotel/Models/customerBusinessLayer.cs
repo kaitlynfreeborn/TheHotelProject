53cd2892d7fa68ce0abccbaae3c3557f34bbb1f1
Leila Abu-Robb <Leila.Abu-Robb@ou.edu>
2019-04-25 09:19:24 -0500

    adding reservations sp

--- KL_Hotel/KL_Hotel/Models/customerBusinessLayer.cs
+++ KL_Hotel/KL_Hotel/Models/customerBusinessLayer.cs
@@ -12,13 +12,113 @@ namespace KL_Hotel.Models
     public class customerBusinessLayer
 
     {
+        public void EditReservation(Reservation res)
+        {
+            String connString = ConfigurationManager.ConnectionStrings["AddResInfo"].ConnectionString;
+            using (SqlConnection sqlCon = new SqlConnection(connString))
+            {
+                SqlCommand command = new SqlCommand("spEditReservation", sqlCon);
+                command.CommandType = CommandType.StoredProcedure;
+                //add the parameters to the command object. 
+
+                SqlParameter paramFirstName = new SqlParameter
+                {
+                    ParameterName = "@FirstName",
+                    Value = cust.FirstName
+                };
+
+                command.Parameters.Add(paramFirstName);
+
+                SqlParameter paramCustomerID = new SqlParameter
+                {
+                    ParameterName = "@CustomerId",
+                    Value = cust.CustomerID
+                };
+                command.Parameters.Add(paramCustomerID);
+
+
+                SqlParameter paramLastName = new SqlParameter
+                {
+                    ParameterName = "@LastName",
+                    Value = cust.LastName
+                };
+                command.Parameters.Add(paramLastName);
+
+                SqlParameter paramUserName = new SqlParameter
+                {
+                    ParameterName = "@UserName",
+                    Value = cust.UserName
+                };
+                command.Parameters.Add(paramUserName);
+
+                SqlParameter paramPassword = new SqlParameter
+                {
+                    ParameterName = "@Password",
+                    Value = cust.Password
+                };
+                command.Parameters.Add(paramPassword);
+
+
+                //open the connection
+                sqlCon.Open();
+                //execute the procedure
+                command.ExecuteNonQuery();
+            }
+        }
+        public void AddReservation(Reservation res)
+        {
+            String connString = ConfigurationManager.ConnectionStrings["AddResInfo"].ConnectionString;
+            using (SqlConnection sqlCon = new SqlConnection(connString))
+            {
+                SqlCommand command = new SqlCommand("spAddReservation", sqlCon)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+
+                SqlParameter paramFirstName = new SqlParameter
+                {
+                    ParameterName = "@FirstName",
+                    Value = cust.FirstName
+                };
+
+                command.Parameters.Add(paramFirstName);
+
+                SqlParameter paramLastName = new SqlParameter
+                {
+                    ParameterName = "@LastName",
+                    Value = cust.LastName
+                };
+                command.Parameters.Add(paramLastName);
+
+                SqlParameter paramUserName = new SqlParameter
+                {
+                    ParameterName = "@UserName",
+                    Value = cust.UserName
+                };
+                command.Parameters.Add(paramUserName);
+
+                SqlParameter paramPassword = new SqlParameter
+                {
+                    ParameterName = "@Password",
+                    Value = cust.Password
+                };
+                command.Parameters.Add(paramPassword);
+
+                //open the connection
+                sqlCon.Open();
+                //execute the procedure
+                command.ExecuteNonQuery();
+            }
+        }
         public void AddCustomer(Customer cust)
         {
             String connString = ConfigurationManager.ConnectionStrings["AddCustInfo"].ConnectionString;
             using (SqlConnection sqlCon = new SqlConnection(connString))
             {
-                SqlCommand command = new SqlCommand("spAddCustomer", sqlCon);
-                command.CommandType = CommandType.StoredProcedure;
+                SqlCommand command = new SqlCommand("spAddCustomer", sqlCon)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
 
                 SqlParameter paramFirstName = new SqlParameter
                 {
@@ -116,19 +216,23 @@ namespace KL_Hotel.Models
                 List<Customer> cust = new List<Customer>();
                 using (SqlConnection connection = new SqlConnection(connString))
                 {
-                    SqlCommand cmd = new SqlCommand("select * from Customer", connection);
-                    cmd.CommandType = CommandType.Text;
+                    SqlCommand cmd = new SqlCommand("select * from Customer", connection)
+                    {
+                        CommandType = CommandType.Text
+                    };
                     connection.Open();
                     //read the info from the database table customer and store it in reader object
                     SqlDataReader reader = cmd.ExecuteReader();
                     while (reader.Read())
                     {
-                        Customer customer = new Customer();
-                        customer.CustomerID = Convert.ToInt32(reader[0]);
-                        customer.FirstName = reader[1].ToString();
-                        customer.LastName = reader[2].ToString();
-                        customer.UserName = reader[3].ToString();
-                        customer.Password = reader[4].ToString();
+                        Customer customer = new Customer
+                        {
+                            CustomerID = Convert.ToInt32(reader[0]),
+                            FirstName = reader[1].ToString(),
+                            LastName = reader[2].ToString(),
+                            UserName = reader[3].ToString(),
+                            Password = reader[4].ToString()
+                        };
                         //add the object to the list 
                         cust.Add(customer);
 
