d9279b1e20c23e163afa008a9c7b15bb7e581700
Leila Abu-Robb <Leila.Abu-Robb@ou.edu>
2019-04-25 11:02:26 -0500

    trying to add a calendar

--- KL_Hotel/KL_Hotel/Models/Reservations.cs
+++ KL_Hotel/KL_Hotel/Models/Reservations.cs
@@ -8,6 +8,7 @@ namespace KL_Hotel.Models
     public class Reservations
     {
         public int ReservationID { get; set; }
+        public int CustomerID { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public string RoomType { get; set; }
