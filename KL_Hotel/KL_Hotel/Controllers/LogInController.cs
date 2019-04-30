684997487e5f7b677cfb64b7faceaa8522b73ce5
Leila Abu-Robb <Leila.Abu-Robb@ou.edu>
2019-04-30 11:05:11 -0500

    trying to fix login

---
+++ KL_Hotel/KL_Hotel/Controllers/LogInController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KL_Hotel.Controllers
+{
+    public class LogInController : Controller
+    {
+        // GET: LogIn
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+    [HttpPost]
+    public ActionResult IndexLogInModel model)
+    {
+         cbe = new ConsumerBankingEntities();
+        var s = cbe.GetCBLoginInfo(model.UserName, model.Password);
+
+        var item = s.FirstOrDefault();
+        if (item == "Success")
+        {
+
+            return View("UserLandingView");
+        }
+        else if (item == "User Does not Exists")
+
+        {
+            ViewBag.NotValidUser = item;
+
+        }
+        else
+        {
+            ViewBag.Failedcount = item;
+        }
+
+        return View("Index");
+    }
+}
