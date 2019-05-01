60c9e3e8d0906542722963bb3f7e7916d5445a85
Leila Abu-Robb <Leila.Abu-Robb@ou.edu>
2019-04-30 19:03:01 -0500

    few

--- KL_Hotel/KL_Hotel/Models/Reservations.cs
+++ KL_Hotel/KL_Hotel/Models/Reservations.cs
@@ -13,9 +13,5 @@ namespace KL_Hotel.Models
         public DateTime EndDate { get; set; }
         public string RoomType { get; set; }
 
-        public static implicit operator Reservations(Reservations v)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
